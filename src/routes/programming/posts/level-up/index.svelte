294e00ecc0452c665ff04edf8732b3e6b297e3ba
raymondboswel <raymondboswel@gmail.com>
2021-08-09 20:12:47 +0200

    Added navigation hints

--- src/routes/programming/posts/level-up/index.svelte
+++ src/routes/programming/posts/level-up/index.svelte
@@ -6,5 +6,6 @@
   <header class="text-3xl font-bold text-center">Level up your programming </header>
   <section class="mx-8 mt-4 rounded border">
   <img src="{assets}/super-mario.jpg" />
-  </section>
+</section>
+<div class="mx-8 text-right">(Use the left and right arrows to navigate)</div>
 </article>
